248fcb7ec58cc47b7b4f5c0a918bb56983b2fde8
Tamas Sallai <tamas.sallai.hu@gmail.com>
2024-02-26 23:35:07 +0100

    extended .env types

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,11 +1,13 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      CORS_ORIGIN: string
       PORT: string
       DATABASE_URL: string
       REDIS_URL: string
       SESSION_SECRET: string
-      CORS_ORIGIN: string
+      EMAIL_SECRET: string
+      SENDGRID_API_KEY: string
     }
   }
 }
