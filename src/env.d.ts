0a9ae19f35a902d4572733003efd9f3a8da33deb
Tamas Sallai <tamas.sallai.hu@gmail.com>
2024-05-13 12:37:24 +0200

    added redis url

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,23 @@
 import { SessionUser } from './types'
 import { FastifySessionObject } from '@fastify/session'
 
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT: string
+      HOST: string
+      REDIS_URL: string
+      DATABASE_URL: string
+      COOKIE_SECRET: string
+      COOKIE_TTL: string
+    }
+  }
+}
+
 declare module '@fastify/session' {
   interface FastifySessionObject {
     user?: SessionUser
   }
 }
 
-export default {}
+export {}
